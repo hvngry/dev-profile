96526eb3dd0146e8f194ac462546fa996ea38bce
pupmiIk <joee.saavedra@gmail.com>
2024-08-27 17:46:46 +0800

    testing

---
+++ src/pages/home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Pic from "../assets/images/me.jpg";
+
+const Home = () => {
+  return (
+    <div>
+      <img src={ Pic } alt="image of me"/>
+      <h2>hi</h2>
+    </div>
+  );
+}
+
+export default Home;
